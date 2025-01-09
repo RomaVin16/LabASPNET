d3c12f54feb680d94650cdeee39fbb95d6ee53b4
RomaVin16 <159806983+RomaVin16@users.noreply.github.com>
2025-01-09 16:24:08 +0500

    Finish

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,103 @@
+﻿using LabASPNET.Models;
+using LabASPNET.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LabASPNET.Controllers
+{
+    public class OrdersController: Controller
+    {
+        private readonly RepositoryContext _context;
+
+        public OrdersController(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var orders = _context.Orders.ToList();
+
+            return View(orders);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Orders.Add(order);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(order);
+        }
+
+        [HttpPost("EditTable")]
+        public IActionResult EditTable(Dictionary<int, Order> orders)
+        {
+            if (orders == null || orders.Count == 0)
+            {
+                return BadRequest("No orders to edit");
+            }
+
+            var dbOrders = _context.Orders.ToList();
+            foreach (var orderEntry in orders)
+            {
+                var id = orderEntry.Key;
+                var order = orderEntry.Value;
+
+                var dbOrder = dbOrders.FirstOrDefault(o => o.OrderId == id);
+                if (dbOrder == null)
+                {
+                    ModelState.AddModelError($"order", $"Can not find order with Id: {order.OrderId}");
+                    continue;
+                }
+
+                dbOrder.UserId = order.UserId;
+                dbOrder.ProductId = order.ProductId;
+                dbOrder.Quantity = order.Quantity;
+                dbOrder.OrderDate = order.OrderDate;
+
+                if (order.OrderDate.Kind == DateTimeKind.Unspecified)
+                {
+                    dbOrder.OrderDate = DateTime.SpecifyKind(order.OrderDate, DateTimeKind.Utc);
+                }
+                else if (order.OrderDate.Kind == DateTimeKind.Local)
+                {
+                    dbOrder.OrderDate = order.OrderDate.ToUniversalTime();
+                }
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost("DeleteOrder/{id}")]
+        public IActionResult DeleteOrder(int id)
+        {
+            var order = _context.Orders.FirstOrDefault(o => o.OrderId == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            _context.Orders.Remove(order);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,96 @@
+﻿using LabASPNET.Models;
+using LabASPNET.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LabASPNET.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly RepositoryContext _context;
+
+        public ProductsController(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        public ActionResult Index()
+        {
+            var products = _context.Products.ToList();
+
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Products.Add(product);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+
+        [HttpGet("EditProduct/{id}")]
+        public IActionResult EditProduct(int id)
+        {
+            var product = _context.Products.FirstOrDefault(p => p.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product); 
+        }
+
+        [HttpPost("EditProduct/{id}")]
+        public IActionResult EditProduct(int id, Product product)
+        {
+            if (id != product.ProductId)
+            {
+                return BadRequest();
+            }
+
+            var dbProduct = _context.Products.FirstOrDefault(p => p.ProductId == id);
+            if (dbProduct == null)
+            {
+                return NotFound();
+            }
+
+            dbProduct.Name = product.Name;
+            dbProduct.Price = product.Price;
+            dbProduct.Description = product.Description;
+
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost("Delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            var product = _context.Products.FirstOrDefault(p => p.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            var orders = _context.Orders.Where(o => o.ProductId == id).ToList();
+            _context.Orders.RemoveRange(orders);
+
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
